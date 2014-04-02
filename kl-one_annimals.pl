e22c3ce9d8621b89f4f8d78eaf4da9e7e85cf2ab
fokke.dekker@me.com <fokke.dekker@me.com>
2014-04-02 20:57:09 +0200

    added functionality to find animals by features

--- kl-one_annimals.pl
+++ kl-one_annimals.pl
@@ -39,7 +39,7 @@ hasa(mouse,leg,2,2).
 % tails
 hasa(feline,tail,1,1).
 hasa(reptile,tail,1,1).
-hasa(goldfish,tail,1,1).
+hasa(gold-fish,tail,1,1).
 hasa(dolphin,tail,1,1).
 hasa(elephant,tail,1,1).
 
@@ -50,10 +50,7 @@ hasa(gold-fish,gills,2,nil).
 hasa(gold-fish, fin,3,nil).
 hasa(dolphin,3,nil).
 
-
-
-% Test for animal inheritence. 
-
+% Predicates to test for inheritence
 wrapertestIsa(X,[X|Isalist]):-
 testForisa(X, Isalist).
 
@@ -63,11 +60,18 @@ testForisa(X, [Y|IsaList]):-
 isa(X, Y),
 testForisa(Y, IsaList).
 
-% Check for animal features
+% Predicates to find al associated features
 testForHasa([],[]).
 
 testForHasa([NewAnimal|NewIsaList], HasaList):-
 findall(Feature:Lower:Uper, hasa(NewAnimal,Feature,Lower,Uper), InheritedHasaList),
 testForHasa(NewIsaList, NewHasaList),
 append(NewHasaList,InheritedHasaList, HasaList).
+
+getAnimals([],[]).
+
+getAnimals([Feature:Lower:Upper|FeatureList], AnimalList):-
+findall(Animal, hasa(Animal,Feature,Lower,Upper), NewAnimalList),
+getAnimals(FeatureList, TotalAnimalList),
+append(TotalAnimalList,NewAnimalList, AnimalList).
 
