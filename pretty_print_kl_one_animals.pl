94bdb01894d48447d7dd9da91dc48b1720f4eb1f
fokke.dekker@me.com <fokke.dekker@me.com>
2014-04-03 14:54:04 +0200

    added pretty print file

---
+++ pretty_print_kl_one_animals.pl
@@ -0,0 +1,10 @@
+% Jonas Lodewegen (10203745)
+% Menno van Leeuwen (10280588)
+% Fokke Dekker (10264256)
+% Practicum opdracht 1
+% maart 2014
+
+% Import all needed files
+:- consult('kl-one-annimals.pl').
+
+
