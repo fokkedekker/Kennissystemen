f3049b9039eb9a1f80b962875c375ada8df99d89
fokke.dekker@me.com <fokke.dekker@me.com>
2014-04-03 14:51:27 +0200

    moved database to a seperate file

--- kl-one_annimals.pl
+++ kl-one_annimals.pl
@@ -4,51 +4,8 @@
 % Practicum opdracht 1
 % maart 2014
 
-%Generic concepts
-isa(animal, thing).
-isa(mammal, animal).
-isa(nonMammal, animal).
-
-isa(feline, mammal).
-isa(cat, feline).
-isa(lion, feline).
-
-isa(bat, mammal).
-isa(mouse, mammal).
-isa(dolphin, mammal).
-isa(elephant, mammal).
-
-isa(gold-fish, nonMammal).
-
-isa(reptile, nonMammal).
-isa(croc, reptile).
-isa(snake, reptile).
-
-% Relations
-hasa(bird,wing,2,2).
-hasa(bat,wing,2,2).
-
-% Legs
-hasa(feline,leg,4,4).
-hasa(bird,leg,2,2).
-hasa(elephant,leg,4,4).
-hasa(croc,leg,4,4).
-hasa(bat,leg,2,2).
-hasa(mouse,leg,2,2).
-
-% tails
-hasa(feline,tail,1,1).
-hasa(reptile,tail,1,1).
-hasa(gold-fish,tail,1,1).
-hasa(dolphin,tail,1,1).
-hasa(elephant,tail,1,1).
-
-% gills
-hasa(gold-fish,gills,2,nil).
-
-% finss
-hasa(gold-fish, fin,3,nil).
-hasa(dolphin,fin,3,nil).
+%Consult database
+:- consult('database.pl').
 
 % Predicates to test for inheritence
 wrapertestIsa(X,[X|Isalist]):-
