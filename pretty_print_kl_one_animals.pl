a9401f4ef5d3f02eefac31094d126418d56b6abd
Jonas Lodewegen <jonaslodewegen@hotmail.com>
2014-04-04 07:41:45 +0200

    Dik getuned

--- pretty_print_kl_one_animals.pl
+++ pretty_print_kl_one_animals.pl
@@ -5,6 +5,46 @@
 % maart 2014
 
 % Import all needed files
-:- consult('kl-one-annimals.pl').
+:- consult('kl-one_annimals.pl').
 
 
+show:-
+	findall(Y, isa(Y,X), All),
+	print(All),
+	print('\n'),
+	show2(All).
+
+
+show2(All):-
+	All=[First|Rest],
+	wrapertestIsa(First, X),
+	X=[SameAnimal|Parents],
+	inheritanceprinter(First, Parents).
+
+inheritanceprinter(Child, []).
+
+inheritanceprinter(Child, [Parent|RestParents]):-	
+	print(Child),
+	print(' is as subsummee of: \n'),
+	print(Parent),
+	print('       \n'),
+	inheritanceprinter(Child, RestParents).
+
+
+
+featureprinter(Animal):-
+	testForHasa([Animal], Features),
+	featureprint(Features).
+%featureprinter(Animal).
+
+%featureprint([]):-
+%	print('\n').
+featureprint([Feature:Lower:Upper|RestFeatures]):-
+	print('\n     has between '),
+	print(Lower),
+	print(' and '),
+	print(Upper),
+	print(' '),
+	print(Feature),
+	print(' \n').
+	featureprint(RestFeatures).
